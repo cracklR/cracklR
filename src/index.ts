7fcf22c7979c92d99fb264fe8534551d4adf3c6a
iSlammedMyKindle <108752129+iSlammedMyKindle@users.noreply.github.com>
2024-08-14 22:18:18 -0400

    Boilerplate code runs successfully!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// Made by the cracklR group in '24!
+// Initialize the bot!
+
 import { Client, GatewayIntentBits } from "discord.js";
 import { readFile, access } from "fs/promises";
 
@@ -9,6 +12,7 @@ try {
   console.error("Unable to read config file! Exiting...", err);
 }
 
+// If this needs to be in a separate file later, we can!
 interface configFile {
   TOKEN: string;
   DEV_GUILD_ID?: string;
@@ -19,12 +23,27 @@ const config: configFile = JSON.parse(
   (await readFile("./config.json")).toString(),
 );
 
+// TODO: move the list of commands to a separate file or JSON configuration (otherwise the command builder is available if that style is more convenient)
 const commands = [
   {
     name: "ping",
     description: "Replies with Pong!",
   },
 ];
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client?.user?.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+await client.login(config.TOKEN);
 
 try {
   console.log("Started refreshing application (/) commands.");
@@ -46,17 +65,3 @@ try {
 } catch (error) {
   console.error(error);
 }
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client?.user?.tag}!`);
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
-  }
-});
-
-client.login(config.TOKEN);
